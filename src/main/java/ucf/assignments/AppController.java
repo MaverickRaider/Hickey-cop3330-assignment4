bc7e39e1a875b1744f00dbbeee54f803ea2dee1b
MichaelHickey <michaelhickey319@gmail.com>
2021-07-03 20:29:14 -0400

    7/03/2021 8:28pm

---
+++ src/main/java/ucf/assignments/AppController.java
@@ -0,0 +1,148 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class AppController {
+    @FXML
+    private TextField displayListTitle;
+    private TextField displayItemTitle;
+    private TextField displayItemDescription;
+    private TextField displayDueDate;
+
+    @FXML
+    public void viewListClicked(ActionEvent actionEvent) {
+        viewList();
+    }
+
+    public void viewList() {
+        // Check which list is marked with the checkListMarked()
+        // Find TodoList
+        // Display TodoList in new GUI
+    }
+
+    @FXML
+    public void editListClicked(ActionEvent actionEvent) {
+        editList();
+    }
+
+    public void editList() {
+        // Check which list is marked with the checkListMarked()
+        // is nothing checked, ignore input by user
+        // Select title text field to be edited
+        // Display the new title in the text field
+    }
+
+    @FXML
+    public void deleteListClicked(ActionEvent actionEvent) {
+        deleteList();
+    }
+
+    public void deleteList() {
+        // Check which list is marked with the checkListMarked()
+        // is nothing checked, ignore input by user
+        // Delete whole list from the lists
+    }
+
+    @FXML
+    public void newListClicked(ActionEvent actionEvent) {
+        newList();
+    }
+
+    public void newList() {
+        // Create a new list in GUI with the View, Title, and Check Box
+        // Ask for Title
+        // call newTask() as we should make empty lists
+        // Display list
+    }
+
+    @FXML
+    public void checkListMarked(ActionEvent actionEvent) {
+        isListChecked();
+    }
+
+    public void isListChecked() {
+        // Find which list has the check box marked
+        // if any lists are checked off, return true.
+        // if nothing is checked, ignore input
+    }
+
+    @FXML
+    public void saveListClicked(ActionEvent actionEvent) {
+        saveList();
+    }
+
+    public void saveList() {
+        // Check which list is marked with the checkListMarked()
+        // Ask User what they want to name the new file
+        // Write Selected Lists to file
+    }
+
+    @FXML
+    public void editTaskClicked(ActionEvent actionEvent) {
+        editTask();
+    }
+
+    public void editTask() {
+        // Check which task is marked with the checkListMarked()
+        // edit the title of the item first
+        // edit the description of the item next
+        // edit the due date of the item
+    }
+
+    @FXML
+    public void deleteTaskClicked(ActionEvent actionEvent) {
+        deleteTask();
+    }
+
+    public void deleteTask() {
+        // Check which task is marked with the checkListMarked()
+        // remove item from list and GUI
+    }
+
+    @FXML
+    public void checkTaskMarked(ActionEvent actionEvent) {
+        isTaskChecked();
+    }
+
+    public void isTaskChecked() {
+        // Find which list has the check box marked
+        // if any lists are checked off, return true.
+        // if nothing is checked, ignore input
+    }
+
+    @FXML
+    public void saveTaskClicked(ActionEvent actionEvent) {
+        saveTask();
+    }
+
+    public void saveTask() {
+        // Check which task is marked with the checkListMarked()
+        // ask user for title
+        // write just item, description and due date to file
+    }
+
+    @FXML
+    public void newTaskClicked(ActionEvent actionEvent) {
+        newTask();
+    }
+
+    public void newTask() {
+        // Generate a new item in the list
+        // ask for item title
+        // ask for description
+        // ask for due date
+    }
+
+    @FXML
+    public void completed(ActionEvent actionEvent) {
+        completedTask();
+    }
+
+    public void completedTask() {
+        // Check which item is marked with the checkListMarked()
+        // set value 'complete' to item
+    }
+}
---
+++ src/test/java/ucf/assignments/AppControllerTest.java
@@ -0,0 +1,118 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppControllerTest {
+
+
+    @Test
+    void viewList() {
+        // get list
+        // open Task fmxl showing the list
+    }
+
+    @Test
+    void editList() {
+        // get list
+        // ask user for title
+        // change title to new string
+
+    }
+
+    @Test
+    void deleteList() {
+        // generate mock list with dummy values
+        // remove list and all items
+        // check to see if the list is gone
+        // if list is gone, return true
+
+    }
+
+    @Test
+    void newList() {
+        // generate new list
+        // Ask for title
+        // Display empty list
+
+    }
+
+    @Test
+    void isListChecked() {
+        // click check box
+        // check if box has a check
+        // return results
+
+    }
+
+    @Test
+    void saveList() {
+        // generate new list to be saved
+        // get list title
+        // get all items
+        // get each item title, description, duedate
+        // find if each item is completed or not
+        // make a new file called 'test_save.txt'
+        // write list to file
+
+    }
+
+    @Test
+    void editTask() {
+        // click on edit
+        // ask for new item title
+        // ask for new description
+        // ask for new due date
+        // assert values into item
+        // display item
+
+    }
+
+    @Test
+    void deleteTask() {
+        // generate new list with an item to be deleted
+        // fill item with dummy values
+        // display item
+        // remove the item
+        // display list
+
+    }
+
+    @Test
+    void isTaskChecked() {
+        // click check box
+        // check if box has a check
+        // return results
+    }
+
+    @Test
+    void saveTask() {
+        // generate new item to be saved
+        // get each item title, description, due date
+        // find if each item is completed or not
+        // make a new file called 'test_item_save.txt'
+        // write item and info to file
+    }
+
+    @Test
+    void newTask() {
+        // generate new item
+        // ask for item title
+        // ask for description
+        // ask for due date
+        // leave completed unchecked
+        // display new item
+
+    }
+
+    @Test
+    void completedTask() {
+        // click check box
+        // if not checked, return completed as true
+        // if already checked, return completed as false
+        // display item and completed value
+
+    }
+
+}
